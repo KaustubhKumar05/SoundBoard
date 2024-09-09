fd174ba0e924757e7d389d3d01bf83f58da032dd
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2024-09-09 23:06:47 +0530

    update: todos

--- client/src/components/Sidebar/Todos.tsx
+++ client/src/components/Sidebar/Todos.tsx
@@ -4,10 +4,11 @@ export const Todos = () => (
   <div>
     <h2 className="font-mono text-lg">Todos</h2>
     <ul className="px-4">
-      <li className="list-disc">CRUD using Redis</li>
-      <li className="list-disc">Script and save tracks</li>
+      <li className="list-disc">
+        Record and save tracks: sidebar collapses, bottom bar rises
+      </li>
+      <li>Track CRUD</li>
       <li className="list-disc">Convert yt track to a board</li>
-      <li className="list-disc">Fork tracks and boards</li>
       <li className="list-disc">Customisable UI</li>
     </ul>
   </div>
