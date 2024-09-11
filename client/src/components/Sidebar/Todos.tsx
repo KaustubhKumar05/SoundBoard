ee7710982eece6efa2f27f6128b952a7206b25ee
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2024-09-12 01:07:32 +0530

    update: todos

--- client/src/components/Sidebar/Todos.tsx
+++ client/src/components/Sidebar/Todos.tsx
@@ -5,15 +5,17 @@ export const Todos = () => (
     <h2 className="font-mono text-lg">Todos</h2>
     <ul className="px-4">
 
-      General improvements: Sept
+      General improvements:
       <li className="list-disc">Dependabot alerts</li>
       <li className="list-disc">Secure backend</li>
       <li className="list-disc">Log actions</li>
+      <li className="list-disc">Space to reset all buttons playing - checkbox option to hide</li>
       <li className="list-disc">Welcome toasts - click to update keys, drag and drop</li>
       <li className="list-disc">Better toasts, common error message file</li>
+----------------------------------------------------------------
       <li className="list-disc">Routes, loading screen</li>
       <li className="list-disc">Handle misclicks - sticky keys</li>
-      <li className="list-disc">Clear loops on mode change</li>
+      <li className="list-disc">Clear loops on mode change - spacebar can handle this</li>
       <li className="list-disc">Hide irrelevant options while editing buttons - update, delete, cancel</li>
       <li className="list-disc">SEO, Favicon</li>
 
@@ -21,18 +23,19 @@ export const Todos = () => (
       <li className="list-disc">
         Recording UI - rolling window, buttons: mode | start pause | stop
       </li>
-      <li className="list-disc">Tracks CRUD</li>
+      <li className="list-disc">Tracks CRUD - 1m duration</li>
       <li className="list-disc">Overlap tracks</li>
 
       Phase 3: Oct
       <li className="list-disc">All tracks button in sidepane loads player UI</li>
-      <li className="list-disc">Player mode</li>
+      <li className="list-disc">Player mode - spinning record</li>
       <li className="list-disc">Convert yt track to a board</li>
       <li className="list-disc">Customisable UI</li>
 
       Final touches: Oct
       <li className="list-disc">Google sign in - not needed to listen, test out a board</li>
       <li className="list-disc">Redis setex</li>
+      <li className="list-disc">Redirect mobile users to player UI with top tracks</li>
       <li className="list-disc">Limits</li>
       <li className="list-disc">Umami analytics</li>
       <li className="list-disc">Star boards, tracks</li>
